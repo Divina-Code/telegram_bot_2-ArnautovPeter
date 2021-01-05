9b6ae6560e76313f882db30626db3d425bf6432f
ArnautovPeter <j32arnautov@gmail.com>
2021-01-05 13:36:16 +0500

    Теперь можно сыграть в виселицу

--- 16 telgram_bot.py
+++ 16 telgram_bot.py
@@ -1,7 +1,15 @@
 import telebot
 from MyToken import TOKEN
 from random import randint
+import random
 
+s = False
+s2 = False
+word_list = ('автострада','спасибо')
+word0 = random.choice(word_list)
+word = list(word0)
+ans = list("*" * len(word0))
+kol = 0
 
 bot = telebot.TeleBot(TOKEN)  #Создаём подключение к боту
 
@@ -42,15 +50,59 @@ def create_password(message):
 @bot.message_handler(commands=['reverse'])
 
 def reverse_message(message):
-    bot.send_message(message.chat.id, message.text[:8:-1])
+    global s
+    s = True
+
+@bot.message_handler(commands=['play'])
+
+def play_gallows(message):
+    global s2
+    s2 = True
+    bot.send_message(message.chat.id, "*" * len(word))
+
+@bot.message_handler(commands=['start'])
+
+def bot_start(message):
+    bot.send_message(message.chat.id, "Привет! Я умею переворачивать слова и создавать пароли.")
+    bot.send_message(message.chat.id, "/password")
+    bot.send_message(message.chat.id, "/reverse")
+
 
 @bot.message_handler(content_types=['text'])
 
 def otvet_na_text(message):
     print(message)
-    bot.send_message(message.chat.id, "Привет! Я умею переворачивать слова и создавать пароли.")
-    bot.send_message(message.chat.id, "/password")
-    bot.send_message(message.chat.id, "/reverse your text")
+    global s, s2, kol
+    if (s == True):
+        bot.send_message(message.chat.id, message.text[::-1])
+        s = False
+    elif (s2 == True):
+        if (message.text in word):
+            for i in range(len(word)):
+                if(word[i] == message.text):
+                    ans[i] = message.text
+        else:
+            kol += 1
+            bot.send_message(message.chat.id, "Этой буквы нет.")
+
+        ans_s = ""
+        for i in range(len(ans)):
+            ans_s += str(ans[i])
+
+        bot.send_message(message.chat.id, ans_s)
+        bot.send_message(message.chat.id, "Количество ошибок: " + str(kol))
+
+        if (ans == word):
+            bot.send_message(message.chat.id, "Вы выиграли.")
+            bot.send_message(message.chat.id, "Поздравляю")
+            kol = 0
+
+        if (kol == 5):
+            bot.send_message(message.chat.id, "Вы проиграли.")
+            bot.send_message(message.chat.id, "Удачи в следующий раз.")
+            s2 = False
+    else:
+        bot.send_message(message.chat.id, "Введите команду.")
 
 #vot nazvanie ArnautovPeter_FirstBot
 
